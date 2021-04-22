b2ebc75401b28f6bbbce7c4291ccd1705ff932ce
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-04-21 18:35:16 -0700

    Added html style and js

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+const texts = ["Full stack developer"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+(function type() {
+    // console.log("type function called");
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".type").textContent = letter;
+    // console.log(letter);
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+}());
